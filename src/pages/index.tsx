67112a30b574bbcdf2ba03fb58348b7a338738eb
nabi.lee <hello@nabi.kim>
2024-05-18 16:11:23 +0900

    feat: 메인 페이지를 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,52 @@
 import type { ReactNode } from 'react'
+import { Button, Center, Text, VStack } from '@channel.io/bezier-react'
+import { BookCoverIcon } from '@channel.io/bezier-icons'
+import Link from 'next/link'
 import { BasicLayout } from '@/layout/BasicLayout'
 
 export default function Page() {
-  return <div>Hello, World!</div>
+  return (
+    <Center height="100vh">
+      <VStack
+        spacing={16}
+        maxWidth={300}
+        width="100%"
+      >
+        <Text
+          typo="24"
+          bold
+        >
+          Diary
+        </Text>
+        <Text color="txt-black-darker">쉽고 재미있게 일상을 기록하세요</Text>
+
+        <Link
+          href="/diary/create-user"
+          legacyBehavior
+        >
+          <Button
+            size="l"
+            colorVariant="monochrome-dark"
+            styleVariant="secondary"
+            leftContent={BookCoverIcon}
+            text="나만의 일기 만들기"
+          />
+        </Link>
+
+        <Link
+          href="/diary/find-user"
+          legacyBehavior
+        >
+          <Button
+            size="xs"
+            colorVariant="monochrome-dark"
+            styleVariant="tertiary"
+            text="이미 일기를 만들었나요?"
+          />
+        </Link>
+      </VStack>
+    </Center>
+  )
 }
 
 Page.getLayout = (page: ReactNode) => <BasicLayout>{page}</BasicLayout>
